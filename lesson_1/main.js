a7a2fc5712dc53512a7aae6754c43c444257deca
Taras-Shavel <taras.shavel39@gmail.com>
2023-02-01 19:00:56 +0200

    I did homework 1 from lesson_1

---
+++ lesson_1/main.js
@@ -0,0 +1,62 @@
+//Завдання:
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log
+//
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+//
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//     let a = 100; let b = '100'; let c = true;
+//
+// Додаткове для тих хто цікавився prompt`oм
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
+
+//Виконана робоиа:
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log
+let hello = 'hello';
+let owu = 'owu';
+let com = 'com';
+let ua = 'ua';
+let one = 1;
+let ten = 10;
+let nine = -999;
+let one_three = 123;
+const PI = 3.14;
+let two_seven = 2.7;
+let a_true = true;
+let b_false = false;
+console.log(hello, owu, com, ua, one, ten, nine, one_three, PI, two_seven, a_true, b_false);
+
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+
+let firstName = 'Taras'
+let middleName = 'Shavel'
+let lastName = 'Bohdanovich'
+
+let person = (firstName + ' ' + middleName + ' ' + lastName)
+
+console.log(person);
+
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//     let a = 100; let b = '100'; let c = true;
+
+let a = 100;
+let b = '100';
+let c = true;
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c)
+
+// Додаткове для тих хто цікавився prompt`oм
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
+
+//Ім'я
+let name = window.prompt('Ведіть своє імя:');
+console.log(name);
+//По-Батькові
+let surname = window.prompt('Як вас По-Батькові?:');
+console.log(surname);
+//Років
+let age = window.prompt('Скільки вам років?:');
+console.log(age);
